erkemp/kolmafia

commit 3bb9cfeff668c1b5f6898dd4384e9ef6120b4d96

Emergency fix, part 3 - gets llama gongs & items that produce a combat (such as drum machines) working again.  This was done by considering any page loads, that consist solely of JavaScript code that sets the mainpane location, as the moral equivalent of a HTTP redirect (which is how those things were previously handled).
During automatic HP/MP restoration, if an item was purchased but ends up
actually reducing your points, give up on it and try something else.  This
could happen if the purchase required an outfit change, which reduced your
maximum HP/MP below your current value.

Evil golden arches are multi-usable.


git-svn-id: svn://svn.code.sf.net/p/kolmafia/code@7382 b29ace70-8910-0410-8dcc-aa2fc6433167

src/net/sourceforge/kolmafia/KoLmafia.java
@@ -1343,7 +1343,7 @@ public abstract class KoLmafia
 					maximum = ( (Number) maximumMethod.invoke( null, empty ) ).intValue();
 					desired = Math.min( maximum, desired );
 
-					if ( last == current )
+					if ( last >= current )
 					{
 						++indexToTry;
 					}

src/net/sourceforge/kolmafia/request/GenericRequest.java
@@ -112,6 +112,7 @@ public class GenericRequest
 	}
 
 	public static final Pattern REDIRECT_PATTERN = Pattern.compile( "([^\\/]+)\\/(login\\.php.*)", Pattern.DOTALL );
+	public static final Pattern JS_REDIRECT_PATTERN = Pattern.compile( ">\\s*top.mainpane.document.location\\s*=\\s*\"(.*?)\";" );
 
 	public static boolean isRatQuest = false;
 	public static boolean isBarrelSmash = false;
@@ -1240,6 +1241,19 @@ public class GenericRequest
 			{
 				shouldStop = this.retrieveServerReply( istream );
 				istream.close();
+				if ( shouldStop && this.responseText != null &&
+					this.responseText.length() < 100 )
+				{
+					// This may be a JavaScript redirect.
+					Matcher m = GenericRequest.JS_REDIRECT_PATTERN.matcher(
+						this.responseText );
+					if ( m.find() )
+					{
+						this.responseCode = 302;
+						this.redirectLocation = m.group( 1 );
+						shouldStop = this.handleServerRedirect();
+					}
+				}
 			}
 			else
 			{

src/net/sourceforge/kolmafia/request/UseItemRequest.java
@@ -66,6 +66,7 @@ import net.sourceforge.kolmafia.persistence.Preferences;
 import net.sourceforge.kolmafia.persistence.SkillDatabase;
 import net.sourceforge.kolmafia.request.DwarfFactoryRequest;
 import net.sourceforge.kolmafia.request.PyramidRequest;
+import net.sourceforge.kolmafia.session.ChoiceManager;
 import net.sourceforge.kolmafia.session.EquipmentManager;
 import net.sourceforge.kolmafia.session.InventoryManager;
 import net.sourceforge.kolmafia.session.MoodManager;
@@ -1799,7 +1800,7 @@ public class UseItemRequest
 
 			if ( responseText.indexOf( "fight.php" ) != -1 )
 			{
-				GenericRequest.checkItemRedirection( item );
+				// GenericRequest.checkItemRedirection( item );
 			}
 
 			// The item is only consumed once you turn in
@@ -2220,7 +2221,7 @@ public class UseItemRequest
 
 			if ( responseText.indexOf( "fight.php" ) != -1 )
 			{
-				GenericRequest.checkItemRedirection( item );
+				// GenericRequest.checkItemRedirection( item );
 			}
 
 			// Even if we are redirected to a fight, the item is
@@ -2274,7 +2275,7 @@ public class UseItemRequest
 
 			if ( responseText.indexOf( "fight.php" ) != -1 )
 			{
-				GenericRequest.checkItemRedirection( item );
+				// GenericRequest.checkItemRedirection( item );
 			}
 
 			// Even if we are redirected to a fight, the item is
@@ -2319,7 +2320,7 @@ public class UseItemRequest
 
 			if ( responseText.indexOf( "fight.php" ) != -1 )
 			{
-				GenericRequest.checkItemRedirection( item );
+				// GenericRequest.checkItemRedirection( item );
 			}
 
 			// Even if we are redirected to a fight, the item is
@@ -2344,7 +2345,7 @@ public class UseItemRequest
 			// If we are redirected to a fight, the item is
 			// consumed elsewhere
 
-			GenericRequest.checkItemRedirection( item );
+			// GenericRequest.checkItemRedirection( item );
 			ResultProcessor.processResult( item );
 
 			return;
@@ -2828,8 +2829,8 @@ public class UseItemRequest
 
 			if ( responseText.indexOf( "fight.php" ) != -1 )
 			{
-                                GenericRequest.checkItemRedirection( item );
-                        }
+				// GenericRequest.checkItemRedirection( item );
+			}
 
 			return;
 

commit 0fcd6be44ab8d0ea2846d0f64525543bfb28e4bb

Fix problem introduced in r7382 with the use of combat-causing items (such as drum machines) from the relay browser, with the Combat Action Bar enabled. The symptom was that the first round of combat overlaid the mainpane, rather than replacing it.
Don't request a charpane update in response to chat notification that you've
received a buff, if mafia is currently processing a command.  The
asynchronous update might not get processed in the proper order relative to
the updates performed by the command, resulting in an incorrect adventure
count or possibly other problems.


git-svn-id: svn://svn.code.sf.net/p/kolmafia/code@7386 b29ace70-8910-0410-8dcc-aa2fc6433167

src/net/sourceforge/kolmafia/KoLmafia.java
@@ -164,6 +164,7 @@ public abstract class KoLmafia
 
 	public static boolean isMakingRequest = false;
 	public static int continuationState = KoLConstants.CONTINUE_STATE;
+	public static int displayState = KoLConstants.ENABLE_STATE;
 
 	public static final int[] initialStats = new int[ 3 ];
 
@@ -644,6 +645,8 @@ public abstract class KoLmafia
 		{
 			KoLDesktop.getInstance().updateDisplayState( state );
 		}
+		
+		KoLmafia.displayState = state;
 	}
 
 	public static final void enableDisplay()

src/net/sourceforge/kolmafia/request/GenericRequest.java
@@ -1249,7 +1249,6 @@ public class GenericRequest
 						this.responseText );
 					if ( m.find() )
 					{
-						this.responseCode = 302;
 						this.redirectLocation = m.group( 1 );
 						shouldStop = this.handleServerRedirect();
 					}

src/net/sourceforge/kolmafia/session/ChatManager.java
@@ -999,7 +999,13 @@ public abstract class ChatManager
 
 			if ( displayHTML.indexOf( " has " ) != -1 )
 			{
-				RequestThread.postRequest( CharPaneRequest.getInstance() );
+				switch ( KoLmafia.displayState )
+				{
+				case KoLConstants.ABORT_STATE:
+				case KoLConstants.ERROR_STATE:
+				case KoLConstants.ENABLE_STATE:
+					RequestThread.postRequest( CharPaneRequest.getInstance() );
+				}
 			}
 
 			String plainTextEvent = KoLConstants.ANYTAG_PATTERN.matcher( displayHTML ).replaceAll( "" );